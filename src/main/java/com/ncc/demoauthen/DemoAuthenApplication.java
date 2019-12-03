a012bae6f2d801fcb62e364b3290f5d4e0a1bf08
Vu Lenh Tho <darksoulover@gmail.com>
2019-12-03 10:45:43 +0700

    first commit

---
+++ src/main/java/com/ncc/demoauthen/DemoAuthenApplication.java
@@ -0,0 +1,13 @@
+package com.ncc.demoauthen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAuthenApplication.class, args);
+    }
+
+}
